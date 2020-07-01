8998f0cecc6f0987235fc3141732efa2f70d6ac9
davorpa <katelme@gmail.com>
2020-07-01 02:28:23 +0200

    Solving problem 1. Race condition fixed

--- exercise/problem/requests.go
+++ exercise/problem/requests.go
@@ -4,9 +4,12 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
+
 	sites := []string{
 		"https://www.google.com",
 		"https://drive.google.com",
@@ -15,12 +18,22 @@ func main() {
 	}
 
 	for _, site := range sites {
+		wg.Add(1)
+
 		go func(site string) {
+			// `defer` is a way to DRY the sync notification, on an http response error or not.
+			// it force the execution after goroutine function has exit
+			defer wg.Done()
+
 			res, err := http.Get(site)
 			if err != nil {
+				io.WriteString(os.Stderr, site+"\t\t FETCH FAIL: "+err.Error()+"\n")
+				return
 			}
 
-			io.WriteString(os.Stdout, res.Status+"\n")
+			io.WriteString(os.Stdout, site+"\t\t "+res.Status+"\n")
 		}(site)
 	}
+
+	wg.Wait()
 }
