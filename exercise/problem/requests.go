fcdb34136ecaafc6309aafabdbe2759adc736db3
davorpa <katelme@gmail.com>
2020-07-01 12:04:05 +0200

    Solving problem 1. Add more urls to see response variations

--- exercise/problem/requests.go
+++ exercise/problem/requests.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"sync"
 )
 
@@ -15,24 +16,32 @@ func main() {
 		"https://drive.google.com",
 		"https://maps.google.com",
 		"https://hangouts.google.com",
+		"https://www.youtube.com",
+		"https://meetup.com",
+		"https://www.udc.es",
+		"https://github.com/davorpa",
+		"https://www.docker.com",
+		"http://amazon.es",
+		"https://twitter.com",
+		"https://gobyexample.com",
 	}
 
-	for _, site := range sites {
+	for idx, site := range sites {
 		wg.Add(1)
 
-		go func(site string) {
+		go func(site string, idx int) {
 			// `defer` is a way to DRY the sync notification, on an http response error or not.
 			// it force the execution after goroutine function has exit
 			defer wg.Done()
 
 			res, err := http.Get(site)
 			if err != nil {
-				io.WriteString(os.Stderr, site+"\t\t FETCH FAIL: "+err.Error()+"\n")
+				io.WriteString(os.Stderr, strconv.Itoa(idx)+": "+site+"\t\t FETCH FAIL: "+err.Error()+"\n")
 				return
 			}
 
-			io.WriteString(os.Stdout, site+"\t\t "+res.Status+"\n")
-		}(site)
+			io.WriteString(os.Stdout, strconv.Itoa(idx)+": "+site+"\t\t "+res.Status+"\n")
+		}(site, idx)
 	}
 
 	wg.Wait()
